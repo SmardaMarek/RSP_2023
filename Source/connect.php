c8b187b65891e0e4f60392a20c333ac2f9b7b14e
Marek Šmarda <135655698+MarekSmarda001@users.noreply.github.com>
2023-10-22 23:01:18 +0200

    Update connect.php

--- Source/connect.php
+++ Source/connect.php
@@ -4,8 +4,9 @@ $database = "rsp"; // Název vaší databáze
 $username = "root"; // Uživatelské jméno pro přístup k databázi
 $password = ""; // Heslo pro přístup k databázi
 
-try {
-    $dbh = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-} catch (PDOException $e) {
-    die("Chyba připojení k databázi: " . $e->getMessage());
+$con = mysqli_connect($host,$username,$password,$database);
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
 }
