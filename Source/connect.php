3a06ab4b780b7081ad1f6fe18ff48f49344b99a2
Marek Šmarda <135655698+MarekSmarda001@users.noreply.github.com>
2023-10-22 21:39:24 +0200

    local host connect_db
    Config pro připojení k lokální databázi

--- Source/connect.php
+++ Source/connect.php
@@ -1,8 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "xx";
-$password = "xx";
-$dbname = "xx";
-$spojeni = mysqli_connect($servername, $username, $password, $dbname);
- mysqli_set_charset($spojeni, "utf8");
-?>
+$host = "localhost"; // Adresa serveru MySQL
+$database = "rsp"; // Název vaší databáze
+$username = "root"; // Uživatelské jméno pro přístup k databázi
+$password = ""; // Heslo pro přístup k databázi
+
+try {
+    $dbh = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+} catch (PDOException $e) {
+    die("Chyba připojení k databázi: " . $e->getMessage());
+}
